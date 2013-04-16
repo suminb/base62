307470bf1df18b62742750c8a839420fea0b7f9c
Sumin Byeon <suminb@gmail.com>
2013-04-16 15:48:37 -0700

    Package version is automatically reflected.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import base62
 
 setup(name='pybase62',
       py_modules=['base62'],
-      version='0.1.0',
+      version=base62.__version__,
       description='Python module for base62 encoding',
       author='Sumin Byeon',
       author_email='suminb@gmail.com',
