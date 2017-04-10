6b9cebea613386e28ee41ec26fbd2e09d8eafce0
Sumin Byeon <suminb@gmail.com>
2017-04-10 21:57:21 +0900

    Code refactoring

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,6 +3,16 @@ import pytest
 import base62
 
 
+bytes_int_pairs = [
+    (b'\x00', 0),
+    (b'\x01', 1),
+    (b'\x01\x01', 0x0101),
+    (b'\xff\xff', 0xffff),
+    (b'\x01\x01\x01', 0x010101),
+    (b'\x01\x02\x03\x04\x05\x06\x07\x08', 0x0102030405060708),
+]
+
+
 def test_basic():
     assert base62.encode(0) == '0'
     assert base62.decode('0') == 0
@@ -11,26 +21,12 @@ def test_basic():
     assert base62.decode('base62') == 34441886726
 
 
-@pytest.mark.parametrize('b, i', [
-    (b'\x00', 0),
-    (b'\x01', 1),
-    (b'\x01\x01', 0x0101),
-    (b'\xff\xff', 0xffff),
-    (b'\x01\x01\x01', 0x010101),
-    (b'\x01\x02\x03\x04\x05\x06\x07\x08', 0x0102030405060708),
-])
+@pytest.mark.parametrize('b, i', bytes_int_pairs)
 def test_bytes_to_int(b, i):
     assert base62.bytes_to_int(b) == i
 
 
-@pytest.mark.parametrize('b, i', [
-    (b'\x00', 0),
-    (b'\x01', 1),
-    (b'\x01\x01', 0x0101),
-    (b'\xff\xff', 0xffff),
-    (b'\x01\x01\x01', 0x010101),
-    (b'\x01\x02\x03\x04\x05\x06\x07\x08', 0x0102030405060708),
-])
+@pytest.mark.parametrize('b, i', bytes_int_pairs)
 def test_encodebytes(b, i):
     assert base62.encodebytes(b) == base62.encode(i)
 
