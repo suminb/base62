44443129fe41d76fc9c62fe95c66f54b49d65b7a
Woongryong Kim <woongryong@nexon.co.kr>
2017-05-19 19:18:07 +0900

    Add minlen to encode()

--- base62.py
+++ base62.py
@@ -31,22 +31,24 @@ def bytes_to_int(s, byteorder='big', signed=False):
         return sum(ds)
 
 
-def encode(n):
+def encode(n, minlen=0):
     """Encodes a given integer ``n``."""
 
-    s = []
+    chs = []
     while n > 0:
         r = n % BASE
         n //= BASE
 
-        s.append(CHARSET[r])
+        chs.append(CHARSET[r])
 
-    if len(s) > 0:
-        s.reverse()
+    if len(chs) > 0:
+        chs.reverse()
     else:
-        s.append('0')
+        chs.append('0')
 
-    return ''.join(s)
+    s = ''.join(chs)
+    s = CHARSET[0] * max(minlen - len(s), 0) + s
+    return s
 
 
 def encodebytes(s):
