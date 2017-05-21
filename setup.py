714e9363290e5d7f07028152031ba49a2d511a83
Sumin Byeon <suminb@gmail.com>
2017-05-21 17:56:37 +0900

    Put the content of README.rst as a description

--- setup.py
+++ setup.py
@@ -3,10 +3,19 @@
 from distutils.core import setup
 import base62
 
+
+def readme():
+    try:
+        with open('README.rst') as f:
+            return f.read()
+    except:
+        return '(Could not read from README.rst)'
+
+
 setup(name='pybase62',
       py_modules=['base62'],
       version=base62.__version__,
-      description='Python module for base62 encoding',
+      description=readme(),
       author='Sumin Byeon',
       author_email='suminb@gmail.com',
       url='http://github.com/suminb/base62',
