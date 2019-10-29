95620451e0c5518d7a0d2b7fae50399e4dda01f0
Sumin Byeon <suminb@gmail.com>
2019-10-30 01:04:07 +0900

    Reformat code with Black

--- base62.py
+++ base62.py
@@ -6,30 +6,29 @@ base62
 Originated from http://blog.suminb.com/archives/558
 """
 
-__title__ = 'base62'
-__author__ = 'Sumin Byeon'
-__email__ = 'suminb@gmail.com'
-__version__ = '0.4.2'
+__title__ = "base62"
+__author__ = "Sumin Byeon"
+__email__ = "suminb@gmail.com"
+__version__ = "0.4.2"
 
 BASE = 62
-CHARSET_DEFAULT = (
-    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-)
-CHARSET_INVERTED = (
-    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-)
+CHARSET_DEFAULT = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+CHARSET_INVERTED = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 try:
     # NOTE: This is for Python 2. Shall be removed as soon as Python 2 is
     # deprecated.
     string_types = (str, unicode)
-    bytes_types = (bytes, bytearray,)
+    bytes_types = (
+        bytes,
+        bytearray,
+    )
 except NameError:
     string_types = (str,)
     bytes_types = (bytes,)
 
 
-def bytes_to_int(barray, byteorder='big', signed=False):
+def bytes_to_int(barray, byteorder="big", signed=False):
     """Converts a byte array to an integer value.
 
     Python 3 comes with a built-in function to do this, but we would like to
@@ -40,7 +39,7 @@ def bytes_to_int(barray, byteorder='big', signed=False):
         return int.from_bytes(barray, byteorder, signed=signed)
     except AttributeError:
         # For Python 2.x
-        if byteorder != 'big' or signed:
+        if byteorder != "big" or signed:
             raise NotImplementedError()
 
         # NOTE: This won't work if a generator is given
@@ -63,9 +62,9 @@ def encode(n, minlen=1, charset=CHARSET_DEFAULT):
     if len(chs) > 0:
         chs.reverse()
     else:
-        chs.append('0')
+        chs.append("0")
 
-    s = ''.join(chs)
+    s = "".join(chs)
     s = charset[0] * max(minlen - len(s), 0) + s
     return s
 
@@ -90,7 +89,7 @@ def decode(encoded, charset=CHARSET_DEFAULT):
     """
     _check_type(encoded, string_types)
 
-    if encoded.startswith('0z'):
+    if encoded.startswith("0z"):
         encoded = encoded[2:]
 
     l, i, v = len(encoded), 0, 0
@@ -112,7 +111,7 @@ def decodebytes(encoded, charset=CHARSET_DEFAULT):
     decoded = decode(encoded, charset=charset)
     buf = bytearray()
     while decoded > 0:
-        buf.append(decoded & 0xff)
+        buf.append(decoded & 0xFF)
         decoded //= 256
     buf.reverse()
 
@@ -125,13 +124,14 @@ def _value(ch, charset):
     try:
         return charset.index(ch)
     except ValueError:
-        raise ValueError('base62: Invalid character (%s)' % ch)
+        raise ValueError("base62: Invalid character (%s)" % ch)
 
 
 def _check_type(value, expected_type):
     """Checks if the input is in an appropriate type."""
 
     if not isinstance(value, expected_type):
-        msg = 'Expected {} object, not {}'.format(
-            expected_type, value.__class__.__name__)
+        msg = "Expected {} object, not {}".format(
+            expected_type, value.__class__.__name__
+        )
         raise TypeError(msg)
